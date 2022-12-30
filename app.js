09a3d26ccbaada800211956ef8f5cfaee0704242
Quentin Colombier <quent.colombier@gmail.com>
2022-12-30 11:30:19 +0100

    Review authentication and star ratings

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const ExpressError = require("./utils/ExpressError");
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+
+
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users')
 
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -26,8 +32,8 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -44,15 +50,25 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+// Method of authentication (local = mail + password)
+passport.use(new LocalStrategy(User.authenticate()));
+
+// Tell how to store and unstore a user
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '63ac22f41cea83494a2a5d3f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
